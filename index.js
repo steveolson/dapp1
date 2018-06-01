4db1778a7e201678a23473e43fe16dbcbec7e113
Steve Olson <steve@sgsst.com>
2018-05-31 22:33:10 -0400

    minor comment changes

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ var express = require('express')
 var app = express()
 var val = 'Testing';
 
-//
 // process a Get Request
 function doGet (req, res)  {
    res.send('Hi getter man!' + req.param('b'));
    console.log('i got hit! ' + req.param('b'));
 }
 
-//
 // process a Get Request
 function doPost (req, res)  {
   res.send('Hi Poster boy!' + req.param('b'));
   console.log('i got hit! ' + req.param('b'));
 }
 
-// 
 // only once at the start of the service...
 function logStart() {
   console.log('i got it started!');
