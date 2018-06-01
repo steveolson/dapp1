ed6cc288e8086462e688a004f7ac3d644564acf0
Steve Olson <steve@sgsst.com>
2018-05-31 22:19:59 -0400

    rewrite as explicit functions instead of inline functions

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ var express = require('express')
 var app = express()
 var val = 'Testing';
 
-app.get('/', function (req, res) {
-  res.send('Hello dapp1! ' + req.param('b'));
-  // req.get('Content-Type')
-})
+//
+// process a Get Request
+function doGet (req, res)  {
+   res.send('Hi getter man!' + req.param('b'));
+   console.log('i got hit! ' + req.param('b'));
+}
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
-})
+//
+// process a Get Request
+function doPost (req, res)  {
+  res.send('Hi Poster boy!' + req.param('b'));
+  console.log('i got hit! ' + req.param('b'));
+}
+
+// 
+// only once at the start of the service...
+function logStart() {
+  console.log('i got it started!');
+}
+
+app.get('/', doGet);
+app.post('/', doPost);
+app.listen(8000, logStart() );
