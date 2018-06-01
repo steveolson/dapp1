e9ca1a50d14c9df6dbed3211534b43cc8f057b8a
Steve Olson <steve@sgsst.com>
2018-06-01 12:39:59 -0400

    added some spaces

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ function doGet (req, res)  {
    res.send('Hi getter man!' + req.param('b'));
    console.log('i got hit! ' + req.param('b'));
 }
+
 // process a Get Request
 function doPost (req, res)  {
   res.send('Hi Poster boy!' + req.param('b'));
   console.log('i got hit! ' + req.param('b'));
 }
+
 // only once at the start of the service...
 function logStart() {
   console.log('i got it started!');
 }
+
 app.get('/', doGet);
 app.post('/', doPost);
 app.listen(8000, logStart() );
